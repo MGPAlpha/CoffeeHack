3e2683a0942726392e025b1c384bc109ffe3b544
Mitchell Philipp <mgpalpha1@gmail.com>
2024-12-08 02:16:12 -0500

    make fridge/freezer switchable

--- Assets/Scripts/HackingMinigame/HackFreezer.cs
+++ Assets/Scripts/HackingMinigame/HackFreezer.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class HackFreezer : MonoBehaviour
 {
+
+    private SpriteRenderer sp;
+    private Collider2D col;
     
     [SerializeField] private float speedReductionFactor = .15f;
     [SerializeField] private float freezeDamage = .1f;
@@ -12,13 +15,26 @@ public class HackFreezer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        sp = GetComponent<SpriteRenderer>();
+        col = GetComponent<Collider2D>();
+        SetVisibility();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SetVisibility();
+    }
+
+    void SetVisibility() {
+        if (MachineManager._instance.machineStatusDictionary[MachineType.Fridge] == MachineStatus.Hacking) {
+            sp.enabled = true;
+            col.enabled = true;
+        }
+        else {
+            sp.enabled = false;
+            col.enabled = false;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
