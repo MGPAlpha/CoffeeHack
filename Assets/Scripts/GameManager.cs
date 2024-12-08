29b733758b7dc2402ef5ddda9fcff414840faf81
chase.obrien <chaseobrien128@gmail.com>
2024-12-08 15:44:47 -0500

    main menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
@@ -22,6 +23,6 @@ public class GameManager : Singleton<GameManager>
 
     public void LoseGame()
     {
-        Debug.Log("You lose");
+        SceneManager.LoadScene("Main Menu");
     }
 }
